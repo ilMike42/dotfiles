7ef05e1b3b7c8c0f39e2334ec7bef8a1a6afb715
ilMike42 <ilmike_others@icloud.com>
2024-07-24 20:19:18 +0200

    ags: removed css from git (keeping scss)

--- .config/ags/config.js
+++ .config/ags/config.js
@@ -1,23 +1,23 @@
 const entry = App.configDir + '/main.ts'
 const outdir = '/tmp/ags/js'
 
+const reloadCss = () => {
+    // main scss file
+    const scss = `${App.configDir}/styles/style.scss`
+
+    // target css file
+    const css = `${App.configDir}/style.css`
+
+    // compile, reset, apply
+    Utils.exec(`sassc ${scss} ${css}`)
+    App.resetCss()
+    App.applyCss(css)
+};
+
+reloadCss();
+
 Utils.monitorFile(
-    // directory that contains the scss files
-    `${App.configDir}/styles`,
-
-    // reload function
-    function() {
-        // main scss file
-        const scss = `${App.configDir}/styles/style.scss`
-
-        // target css file
-        const css = `${App.configDir}/style.css`
-
-        // compile, reset, apply
-        Utils.exec(`sassc ${scss} ${css}`)
-        App.resetCss()
-        App.applyCss(css)
-    },
+    `${App.configDir}/styles`, reloadCss
 )
 
 
