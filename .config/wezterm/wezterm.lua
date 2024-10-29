856ba9a883bde66de1e7f98e1cb31409e762dae7
ilMike42 <ilmike_others@icloud.com>
2024-10-29 10:09:06 +0100

    wezterm: updated config"

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,23 +1,23 @@
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
 
--- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
+config.font_size = 14
 
--- For example, changing the color scheme:i
-config.color_scheme = "Catppuccin Macchiato"
+config.color_scheme = "catppuccin-macchiato"
 
--- config.use_fancy_tab_bar = false
-config.enable_tab_bar = false
-
--- config.window_decorations = "NONE"
-
--- ********** Background **********
+-- window padding
 config.window_background_opacity = 0.9
+config.enable_tab_bar = false
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
 
-config.automatically_reload_config = true
+-- config.window_decorations = "RESIZE"
 
 -- and finally, return the configuration to wezterm
 return config
